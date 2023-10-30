7bd263f3b0840c3cfd2cfd0f2463e74aac9466ad
Julius C Heitkoetter <juliush@satori-login-001.mit.edu>
2023-10-30 19:50:48 -0400

    deleted setup, to be manually generated from install

--- setup.sh
+++
@@ -1,6 +0,0 @@
-# DO NOT EDIT !! THIS FILE IS GENERATED AT INSTALL (install.sh) !!
-export BASE=/work/submit/juliush/llm_playground
-export OPENAI_API_KEY=`cat /home/submit/juliush/.openai/api.key`
-export PATH=${PATH}:${BASE}/bin
-export PYTHONPATH=${PYTHONPATH}:${BASE}
-
