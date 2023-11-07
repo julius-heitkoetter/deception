114eeba1f66f6e96dbc7db2a8e4a1442d067141f
Julius Heitkoetter <juliush@mit.edu>
2023-11-07 09:22:22 -0500

    adding class frameworks for the main parts of the chain

---
+++ chain.py
@@ -0,0 +1,45 @@
+
+class DataLoader:
+    """
+    Creates qa
+    """
+
+    def __init__(self,):
+        pass
+
+    def __call__(self,):
+        pass
+
+class Deceiver:
+    """
+    qa -> qae
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
+
+
+class Supervisor:
+    """
+    qae -> qaev
+    """
+    
+    def __init__(self,):
+        pass
+
+    def __call__(self,):
+        pass
+
+class Evaluator:
+    """
+    qa -> qav
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
