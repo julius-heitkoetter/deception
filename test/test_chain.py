51a5a1b5d5705d5030992740c3a33367c6e3cd33
Laker Newhouse <lnewhouse@cfs.energy>
2023-11-20 06:59:08 -0500

    Factor run_on_dataset into abstract class

--- test/test_chain.py
+++ test/test_chain.py
@@ -1,9 +1,7 @@
-import sys
-print(sys.path)
 import json
 from lib.models import OpenAILLM
 from config import gpt_35_turbo_base_config
-from lib.chain import Supervisor, Deceiver, Evaluator, run_on_dataset
+from lib.chain import Supervisor, Deceiver, Evaluator
 
 with open("test/data/qa_testsmall_2023-11-08_09_07_32.json", "r") as f:
     dataset = json.load(f)
@@ -17,24 +15,24 @@ print("Dataset at step 0:")
 print(dataset)
 
 # Step 1: create "explanation"
-dataset = run_on_dataset(dataset, deceiver)
+dataset = deceiver.run_on_dataset(dataset)
 print("Dataset at step 1: create explanations")
 print(dataset)
 
-with open("test/data/qae_testsmall", "w") as f:
+with open("test/data/qae_testsmall.json", "w") as f:
     json.dump(dataset, f, indent=4)
 
 # Step 2: create "verdict"
-dataset = run_on_dataset(dataset, supervisor)
+dataset = supervisor.run_on_dataset(dataset)
 print("Dataset at step 2: create verdicts")
 print(dataset)
 
-with open("test/data/qaev_testsmall", "w") as f:
+with open("test/data/qaev_testsmall.json", "w") as f:
     json.dump(dataset, f, indent=4)
 
 # Step 3: create "verdict-without-explanation"
-dataset = run_on_dataset(dataset, evaluator)
-print("Dataset at step 0: create verdicts without explanation")
+dataset = evaluator.run_on_dataset(dataset)
+print("Dataset at step 3: create verdicts without explanation")
 print(dataset)
 
 with open("test/data/qaev_testsmall_complete.json", "w") as f:
