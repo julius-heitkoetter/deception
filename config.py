fb6dad9f03230d4c470e8a8dc599f82b29a52148
Julius Heitkoetter <juliush@mit.edu>
2023-11-27 12:28:36 +0000

    adding llama13b and non RLHF llama models to config

--- config.py
+++ config.py
@@ -51,11 +51,28 @@ evaluator_base_prompt = """
 ################# LLM CONFIGS #################
 ###############################################
 
-llama_base_config = {
+llama_7b_base_config = {
     "base_model": "meta-llama/Llama-2-7b-chat-hf", #the location of the model (ex. meta-llama/Llama-2-70b)
     "peft_model": None, #the location of the finetuning of the model. Can be none
     "quantization": True, #enables 8-bit quantization
-    "max_new_tokens": 4096, #The maximum numbers of tokens to generate
+    "max_new_tokens": 512, #The maximum numbers of tokens to generate
+    "seed": None, #seed value for reproducibility
+    "do_sample": True, #Whether or not to use sampling ; use greedy decoding otherwise.
+    "min_length": None, #The minimum length of the sequence to be generated, input prompt + min_new_tokens
+    "use_cache": True,  #[optional] Whether or not the model should use the past last key/values attentions Whether or not the model should use the past last key/values attentions (if applicable to the model) to speed up decoding.
+    "top_p": .9, # [optional] If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation.
+    "temperature": .6, # [optional] The value used to modulate the next token probabilities.
+    "top_k": 50, # [optional] The number of highest probability vocabulary tokens to keep for top-k-filtering.
+    "repetition_penalty": 1.0, #The parameter for repetition penalty. 1.0 means no penalty.
+    "length_penalty": 1, #[optional] Exponential penalty to the length that is used with beam-based generation.
+    "max_padding_length": None, # the max padding length to be used with tokenizer padding the prompts.
+}
+
+llama_13b_base_config = {
+    "base_model": "meta-llama/Llama-2-13b-chat-hf", #the location of the model (ex. meta-llama/Llama-2-70b)
+    "peft_model": None, #the location of the finetuning of the model. Can be none
+    "quantization": True, #enables 8-bit quantization
+    "max_new_tokens": 512, #The maximum numbers of tokens to generate
     "seed": None, #seed value for reproducibility
     "do_sample": True, #Whether or not to use sampling ; use greedy decoding otherwise.
     "min_length": None, #The minimum length of the sequence to be generated, input prompt + min_new_tokens
@@ -72,7 +89,58 @@ llama_70b_base_config = {
     "base_model": "meta-llama/Llama-2-70b-chat-hf", #the location of the model (ex. meta-llama/Llama-2-70b)
     "peft_model": None, #the location of the finetuning of the model. Can be none
     "quantization": True, #enables 8-bit quantization
-    "max_new_tokens": 4096, #The maximum numbers of tokens to generate
+    "max_new_tokens": 512, #The maximum numbers of tokens to generate
+    "seed": None, #seed value for reproducibility
+    "do_sample": True, #Whether or not to use sampling ; use greedy decoding otherwise.
+    "min_length": None, #The minimum length of the sequence to be generated, input prompt + min_new_tokens
+    "use_cache": True,  #[optional] Whether or not the model should use the past last key/values attentions Whether or not the model should use the past last key/values attentions (if applicable to the model) to speed up decoding.
+    "top_p": .9, # [optional] If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation.
+    "temperature": .6, # [optional] The value used to modulate the next token probabilities.
+    "top_k": 50, # [optional] The number of highest probability vocabulary tokens to keep for top-k-filtering.
+    "repetition_penalty": 1.0, #The parameter for repetition penalty. 1.0 means no penalty.
+    "length_penalty": 1, #[optional] Exponential penalty to the length that is used with beam-based generation.
+    "max_padding_length": None, # the max padding length to be used with tokenizer padding the prompts.
+}
+
+llama_7b_noRLHF_config = {
+    "base_model": "meta-llama/Llama-2-7b-hf", #the location of the model (ex. meta-llama/Llama-2-70b)
+    "peft_model": None, #the location of the finetuning of the model. Can be none
+    "quantization": True, #enables 8-bit quantization
+    "max_new_tokens": 512, #The maximum numbers of tokens to generate
+    "seed": None, #seed value for reproducibility
+    "do_sample": True, #Whether or not to use sampling ; use greedy decoding otherwise.
+    "min_length": None, #The minimum length of the sequence to be generated, input prompt + min_new_tokens
+    "use_cache": True,  #[optional] Whether or not the model should use the past last key/values attentions Whether or not the model should use the past last key/values attentions (if applicable to the model) to speed up decoding.
+    "top_p": .9, # [optional] If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation.
+    "temperature": .6, # [optional] The value used to modulate the next token probabilities.
+    "top_k": 50, # [optional] The number of highest probability vocabulary tokens to keep for top-k-filtering.
+    "repetition_penalty": 1.0, #The parameter for repetition penalty. 1.0 means no penalty.
+    "length_penalty": 1, #[optional] Exponential penalty to the length that is used with beam-based generation.
+    "max_padding_length": None, # the max padding length to be used with tokenizer padding the prompts.
+}
+
+llama_13b_noRLHF_config = {
+    "base_model": "meta-llama/Llama-2-13b-hf", #the location of the model (ex. meta-llama/Llama-2-70b)
+    "peft_model": None, #the location of the finetuning of the model. Can be none
+    "quantization": True, #enables 8-bit quantization
+    "max_new_tokens": 512, #The maximum numbers of tokens to generate
+    "seed": None, #seed value for reproducibility
+    "do_sample": True, #Whether or not to use sampling ; use greedy decoding otherwise.
+    "min_length": None, #The minimum length of the sequence to be generated, input prompt + min_new_tokens
+    "use_cache": True,  #[optional] Whether or not the model should use the past last key/values attentions Whether or not the model should use the past last key/values attentions (if applicable to the model) to speed up decoding.
+    "top_p": .9, # [optional] If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation.
+    "temperature": .6, # [optional] The value used to modulate the next token probabilities.
+    "top_k": 50, # [optional] The number of highest probability vocabulary tokens to keep for top-k-filtering.
+    "repetition_penalty": 1.0, #The parameter for repetition penalty. 1.0 means no penalty.
+    "length_penalty": 1, #[optional] Exponential penalty to the length that is used with beam-based generation.
+    "max_padding_length": None, # the max padding length to be used with tokenizer padding the prompts.
+}
+
+llama_70b_noRLHF_config = {
+    "base_model": "meta-llama/Llama-2-70b-hf", #the location of the model (ex. meta-llama/Llama-2-70b)
+    "peft_model": None, #the location of the finetuning of the model. Can be none
+    "quantization": True, #enables 8-bit quantization
+    "max_new_tokens": 512, #The maximum numbers of tokens to generate
     "seed": None, #seed value for reproducibility
     "do_sample": True, #Whether or not to use sampling ; use greedy decoding otherwise.
     "min_length": None, #The minimum length of the sequence to be generated, input prompt + min_new_tokens
